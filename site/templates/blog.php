8a553b5ea004e30b1abf7f7faaf50fbd99d83190
Dean Verleger <deanverleger@gmail.com>
2020-06-02 21:54:09 -0500

    work on blog

--- site/templates/blog.php
+++ site/templates/blog.php
@@ -1,29 +1,41 @@
 <?php snippet('header') ?>
 
-  <?php $blogUrl = page('blog')->url(); ?>
+  <?php
+    $blogUrl = page('blog')->url();
+    $isCategoryArchive = $isTagArchive = false;
+    if (isset($category)) {
+      $isCategoryArchive = true;
+    } else if (isset($tag)) {
+      $isTagArchive = true;
+    }
+    $isEntryArchive = $isCategoryArchive || $isTagArchive;
+  ?>
   <main class="blog">
     <div class="container">
-
-      <header class="page-header">
-        <h1><?php
-          $isCategoryArchive = $isTagArchive = false;
-          if (!isset($category) && !isset($tag)) {
-            $title = $page->pageTitle()->html();
-            if ($title == '') {
-              $title = $page->title()->html();
+      <div class="header-wrapper">
+        <header class="page-header">
+          <h1><?php
+            if (!$isEntryArchive) {
+              $title = $page->pageTitle()->html();
+              if ($title == '') {
+                $title = $page->title()->html();
+              }
+            } elseif ($isCategoryArchive) {
+              $title = 'Posts filed in <em>' . $category . '</em>';
+            } elseif ($isTagArchive) {
+              $title = 'Posts tagged as <em>' . $tag . '</em>';
             }
-          } elseif (isset($category)) {
-            $isCategoryArchive = true;
-            $title = 'Posts filed in <em>' . $category . '</em>';
-          } elseif (isset($tag)) {
-            $isTagArchive = true;
-            $title = 'Posts tagged as <em>' . $tag . '</em>';
-          }
-          echo $title;
-        ?></h1>
-      </header>
+            echo $title;
+          ?></h1>
+        </header>
+        <?php if ($isEntryArchive): ?>
+        <p><a href="<?= $blogUrl ?>">Return to the blog</a></p>
+        <?php else: ?>
+        <p>Subscribe via <a href="/feed.xml">RSS</a></p>
+        <?php endif; ?>
+      </div>
 
-      <?php if (!$isCategoryArchive && !$isTagArchive && $page->text()->isNotEmpty()): ?>
+      <?php if (!$isEntryArchive && $page->text()->isNotEmpty()): ?>
       <div class="intro">
         <?= $page->text()->kt() ?>
       </div>
@@ -48,7 +60,7 @@
               <div class="meta">
                 <time datetime="<?= $article->date()->toDate('c') ?>" pubdate="pubdate"><?=
                   $article->date()->toDate('M j, Y')
-                ?></time> / <a href="<?= $blogUrl . '/' . urlencode($cat);?>"><?= $cat; ?></a>
+            ?></time><?php if (!$cat->isEmpty()): ?> / <a href="<?= $blogUrl . '/' . urlencode($cat);?>"><?= $cat; ?></a><?php endif; ?>
               </div>
             </article>
 
@@ -71,12 +83,12 @@
           </section>
         </div>
 
-        <?php if ($articles->isNotEmpty()) : ?>
+        <?php if ($articles->isNotEmpty() && !$isEntryArchive) : ?>
         <div class="sub">
 
-          <aside>
-            <h3>Subscribe via <a href="/feed.xml">RSS</a></h3>
-          </aside>
+          <?/* <aside>
+            <h4>Subscribe via <a href="/feed.xml">RSS</a></h4>
+          </aside> */?>
 
           <?php /* <aside>
             <h3>Categories</h3>
@@ -91,8 +103,8 @@
             </ul>
           </aside> */ ?>
 
-          <aside>
-            <h3>Tags</h3>
+          <?php /* <aside>
+            <h4>Tags</h4>
             <ul class="tags">
               <?php foreach($tags as $tag): ?>
               <li>
@@ -102,7 +114,7 @@
               </li>
               <?php endforeach ?>
             </ul>
-          </aside>
+          </aside> */ ?>
         </div>
         <?php endif; ?>
       </div>
