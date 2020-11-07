8859a67a0612ff3d62698ce899e5495c459c7524
Dean Verleger <deanverleger@gmail.com>
2020-11-07 16:46:16 -0600

    add icons

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -39,9 +39,9 @@
 
   <!-- Main Link Tags  -->
 
-  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png">
-  <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48.png">
+  <link href="/favicon-16.png" rel="icon" type="image/png" sizes="16x16">
+  <link href="/favicon-32.png" rel="icon" type="image/png" sizes="32x32">
+  <link href="/favicon-48.png" rel="icon" type="image/png" sizes="48x48">
 
   <!-- iOS  -->
   <link href="/touch-icon-iphone.png" rel="apple-touch-icon">
@@ -53,18 +53,20 @@
   <link href="/touch-icon-start-up-320x480.png" rel="apple-touch-startup-image">
 
   <?php // OPinned Tab ?>
-  <link href="path/to/icon.svg" rel="mask-icon" size="any" color="red">
+  <link href="/icon-opt.svg" rel="mask-icon" size="any" color="<?= $GLOBALS['themeColor']; ?>">
 
   <?php // OAndroid ?>
   <link href="/icon-192.png" rel="icon" sizes="192x192">
   <link href="/icon-128.png" rel="icon" sizes="128x128">
 
   <?php // Others ?>
-  <link href="favicon.icon" rel="shortcut icon" type="image/x-icon">
+  <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon">
 
   <?php // UC Browser ?>
   <link href="/icon-52.png" rel="apple-touch-icon-precomposed" sizes="57x57">
   <link href="/icon-72.png" rel="apple-touch-icon" sizes="72x72">
+
+  <?php // manifest ?>
   <link href="/site.webmanifest" rel="manifest">
 
 <!--`
